dda98aa5b4bc276a60145f864daa22389a5f51f0
jackdi522 <you@example.com>
2017-07-25 12:22:21 +0800

    路由配置 设置 / 路由 不访问 index.html

--- app/app.js
+++ app/app.js
@@ -47,7 +47,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+// 配置 / 路由 不访问 index.html
+app.use(express.static(path.join(__dirname, 'public'),{'index': false}));
 
 app.use(session({
     secret: 'secret',
